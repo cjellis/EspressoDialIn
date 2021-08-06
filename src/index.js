846450ab856e378a1d1182c1108f593ee056ce9f
Craig Ellis <ellis.cr@husky.neu.edu>
2021-08-06 17:19:33 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <head><script data-ad-client="ca-pub-4918387216265589" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script></head>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
@@ -14,4 +15,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
