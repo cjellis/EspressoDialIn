7a4b13762f3f4f0434f999faa4f3ba4f7714b33a
Craig Ellis <ellis.cr@husky.neu.edu>
2021-08-08 21:47:24 -0400

    Fix the stupied spacing for the linter

--- src/listItems.jsx
+++ src/listItems.jsx
@@ -4,17 +4,15 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import LocalCafeIcon from '@material-ui/icons/LocalCafe';
 
-export const mainListItems = () => {
-  return (
-    <div>
-      <ListItem button>
-        <ListItemIcon>
-          <LocalCafeIcon />
-        </ListItemIcon>
-        <ListItemText primary="Track Shots" />
-      </ListItem>
-    </div>
-  );
-};
+export const mainListItems = () => (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <LocalCafeIcon />
+      </ListItemIcon>
+      <ListItemText primary="Track Shots" />
+    </ListItem>
+  </div>
+);
 
 export default mainListItems;
